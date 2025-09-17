1aa720015d73969f15b815c042a2350d0fa7d7b2
den0620 <denis2006d@gmail.com>
2025-09-17 18:00:47 +0300

    add base mahjongg

---
+++ apps/mahjongg.go
@@ -0,0 +1,58 @@
+package apps
+
+import (
+	//"fmt"
+	"riwo/wm"
+	//"strconv"
+	//"syscall/js"
+)
+
+func init() {
+	AppRegistry["Mahjongg"] = mahjonggConstruct
+}
+
+type mahjonggBrick struct {
+	Type	int				// Brick type, identical are deleteable
+	Content	*wm.RiwoObject	// Connected DOM element.
+}
+func createMahjonggBrick(brickType int) *mahjonggBrick {
+	return &mahjonggBrick{
+		Type:    brickType,
+		Content: wm.Create(),
+	}
+}
+
+func mahjonggConstruct(window *wm.RiwoWindow) {
+	//fg := wm.ThemeMap["yellow"]["normal"]
+	//mg := wm.ThemeMap["yellow"]["faded"]
+	bg := wm.ThemeMap["yellow"]["vivid"]
+
+	container := wm.Create()
+	container.
+		Style("height", "100%").
+		Style("display", "flex").
+		Style("flexDirection", "column").
+		Style("justifyContent", "center").
+		Style("alignItems", "center").
+		Style("backgroundColor", bg)
+	
+	examplebrick1 := createMahjonggBrick(1)
+	
+	container.Append(examplebrick1.Content)
+
+	// ...
+
+	window.Content.Inner("").Append(container)
+}
+
+/*
+func applyTheme(e *wm.RiwoObject, theme map[string]string) {
+	e.
+		Style("cursor", wm.CursorInvertUrl).
+		Style("padding", "10px, 20px").
+		Style("color", "#000000").
+		Style("backgroundColor", theme["faded"]).
+		Style("border", "solid "+theme["vivid"]).
+		Style("borderRadius", 0)
+}
+*/
